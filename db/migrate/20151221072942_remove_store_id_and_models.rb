10b346bcc2dffdba5642b55fe9f7c0703a21b9b3
Bart Muzyka <muzykabartek@gmail.com>
2015-12-21 18:09:25 +0800

    Fixed small typo

--- db/migrate/20151221072942_remove_store_id_and_models.rb
+++ db/migrate/20151221072942_remove_store_id_and_models.rb
@@ -3,7 +3,7 @@ class RemoveStoreIdAndModels < ActiveRecord::Migration
     drop_table :spree_store_shipping_methods if table_exists?('spree_store_shipping_methods')
     remove_index :spree_store_shipping_methods, :store_id if index_exists?(:spree_store_shipping_methods, :store_id)
     remove_index :spree_store_shipping_methods, :shipping_method_id if index_exists?(:spree_store_shipping_methods, :shipping_method_id)
-    drop_table :spree_store_payment_methods if table_exists?('spree_store_payment_method')
+    drop_table :spree_store_payment_methods if table_exists?('spree_store_payment_methods')
     drop_table :spree_promotion_rules_stores if table_exists?('spree_promotion_rules_stores')
     remove_column :spree_trackers, :store_id, :integer
   end
